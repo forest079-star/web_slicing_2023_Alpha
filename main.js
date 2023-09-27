20fa19e4f3eb89dd3be139ec638ee8020827427d
forest079 <bluegx08@gmail.com>
2023-09-27 17:37:19 +0800

    add about & gasp & aos & setting fix

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-// 載入 aos
-import './assets/js/aos.js';
 
 // plugin -
+// 載入 aos 套件
+import './assets/js/aos.js';
+import './assets/js/gsap.js';
+
+// import 'aos/dist/aos.css';
 import $ from 'jquery';
 window.jQuery = window.$ = $;
